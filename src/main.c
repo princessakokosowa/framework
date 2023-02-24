cf53a75be608364780d84877f0960b5d64e9e3bb
princessakokosowa <ducrids@gmail.com>
2023-02-24 01:50:55 +0100

    Almost there.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+#ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN	// Exclude rarely-used items from Windows headers.
+#endif
+
+#include "d3d12.h"       // That is DirectX 12.
+// #include "dxcapi.h"      // This, on the other hand, is DirectX Shader Compiler.
+// #include "d3d12shader.h" // That thing provides shader reflection utility.
+
+
 typedef _Bool bool;
 
 #define null  0
@@ -16,41 +25,27 @@ typedef unsigned short     u16;
 typedef unsigned int       u32;
 typedef unsigned long long u64;
 
-typedef float  f32;
-typedef double f64;
+typedef unsigned short f16;
+typedef float          f32;
+typedef double         f64;
 
 typedef size_t    usize;
 typedef ptrdiff_t isize;
 
 #define arraySize(value)                   ((sizeof((value))) / (sizeof((value[0]))))
-#define maybeDefault(value, default_value) (((value) == 0) ? (default_value) : (value))
-#define enumToInt(enum_)                   ((int)(enum_))
+#define enumToInt(value)                   ((isize)(value))
 #define cast(Type, value)                  ((Type)(value))
 
 #define staticAssert(expression)           _Static_assert((expression), "Static assert failed!")
-#define dynamicAssert(expression) (void)(                                        \
-    (!!(expression)) ||                                                              \
-    (_wassert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0) \
+#define dynamicAssert(expression)          (void)(                                \
+    (!!(expression)) ||                                                           \
+    (_wassert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (usize)(__LINE__)), 0) \
     )
 
 #include <stdio.h>
 
-#include "d3d12.h"
-#include "dxgi1_4.h"
-
 int main(void) {
-    bool succeeded = true;
-    
-    ID3D12Debug1* debug_controller = NULL;
-
-    succeeded = SUCCEEDED(D3D12GetDebugInterface(&IID_ID3D12Debug1, &debug_controller));
-    if (succeeded) {
-        ID3D12Debug_EnableDebugLayer(debug_controller);
-        // debug_controller->EnableDebugLayer();
-        // debug_controller->SetEnableGPUBasedValidation(true);
-        // debug_controller->SetEnableSynchronizedCommandQueueValidation(true);
-        // debug_controller->Release();
-    }
+    printf("Hello, world!\n");
 
     return 0;
 }
