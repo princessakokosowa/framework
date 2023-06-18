f472891f86c2862364b97fcc978220688c96a62b
princessakokosowa <ducrids@gmail.com>
2023-06-18 15:28:37 +0200

    Removed gpu.h and gpu_test.h as, at the end of the day, I don't want such placeholders in my codebase.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include "allocator_tests.h"
 #include "array_tests.h"
-#include "gpu_tests.h"
 #include "haversine_tests.h"
 
 int main(void) {
     AllocatorTests_test();
     ArrayTests_test();
-    GpuTests_test();
     HaversineTests_test();
 
     return 0;
