d81b7baa26656cf92128838a3cfc744f2b8777ac
princessakokosowa <ducrids@gmail.com>
2023-06-24 09:54:39 +0200

    Nah, we'll stick to `entryPoint`.

--- src/os/entry_point_win32.c
+++ src/os/entry_point_win32.c
@@ -1,10 +1,10 @@
-function void app(void);
+function void entryPoint(void);
 
 #if BUILD_CONSOLE_OUTPUT
     int main(int argument_count, char **arguments) {
         os_win32_instance = GetModuleHandle(0);
 
-        Os_entryPoint(app, cast(u64, argument_count), arguments);
+        Os_entryPoint(entryPoint, cast(u64, argument_count), arguments);
 
         return 0;
     }
@@ -12,7 +12,7 @@ function void app(void);
     int WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR lp_cmd_line, int n_show_cmd) {
         os_win32_instance = instance;
 
-        Os_entryPoint(app, cast(u64, __argc), __argv);
+        Os_entryPoint(entryPoint, cast(u64, __argc), __argv);
 
         return 0;
     }
